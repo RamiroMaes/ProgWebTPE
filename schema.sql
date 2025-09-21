42222d09fc08ac18805b8a3b82033f75598711f1
Ramiro Maestrojuan <rmaestrojuan@alumnos.exa.unicen.edu.ar>
2025-09-21 17:30:03 -0300

    listo el schema.sql

---
+++ schema.sql
@@ -0,0 +1,94 @@
+CREATE TABLE Club (
+    Nombre varchar(40)  NOT NULL,
+    Ciudad varchar(40)  NOT NULL,
+    CONSTRAINT Club_pk PRIMARY KEY (Nombre,Ciudad)
+);
+
+-- Table: Jugador
+CREATE TABLE Jugador (
+    Nombre varchar(40)  NOT NULL,
+    iD_Jugador int  NOT NULL,
+    Posicion varchar(40)  NOT NULL,
+    Edad int  NOT NULL,
+    fecha_nacimiento date  NOT NULL,
+    altura decimal(1,2)  NOT NULL,
+    Pais_Nombre varchar(40)  NOT NULL,
+    CONSTRAINT Jugador PRIMARY KEY (Nombre,iD_Jugador)
+);
+
+-- Table: Jugo
+CREATE TABLE Jugo (
+    fecha_inicio date  NOT NULL,
+    fecha_fin date  NOT NULL,
+    Jugador_Nombre varchar(40)  NOT NULL,
+    Jugador_iD_Jugador int  NOT NULL,
+    Club_Nombre varchar(40)  NOT NULL,
+    Club_Ciudad varchar(40)  NOT NULL,
+    CONSTRAINT Jugo_en PRIMARY KEY (Jugador_Nombre,Jugador_iD_Jugador,Club_Nombre,Club_Ciudad)
+);
+
+-- Table: Lesion
+CREATE TABLE Lesion (
+    Tipo_Lesion varchar(50)  NOT NULL,
+    Descripcion varchar(100)  NOT NULL,
+    CONSTRAINT Lesion PRIMARY KEY (Tipo_Lesion)
+);
+
+-- Table: Pais
+CREATE TABLE Pais (
+    Nombre varchar(40)  NOT NULL,
+    CONSTRAINT Nombre PRIMARY KEY (Nombre)
+);
+
+-- Table: Tiene
+CREATE TABLE Tiene (
+    fecha_inicio date  NOT NULL,
+    fecha_fin date  NOT NULL,
+    Jugador_Nombre varchar(40)  NOT NULL,
+    Jugador_iD_Jugador int  NOT NULL,
+    Lesion_Tipo_Lesion varchar(50)  NOT NULL,
+    CONSTRAINT Tiene_Lesion PRIMARY KEY (Jugador_Nombre,Jugador_iD_Jugador,Lesion_Tipo_Lesion)
+);
+
+-- foreign keys
+-- Reference: Jugador_Pais (table: Jugador)
+ALTER TABLE Jugador ADD CONSTRAINT Jugador_Pais
+    FOREIGN KEY (Pais_Nombre)
+    REFERENCES Pais (Nombre)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Jugo_Club (table: Jugo)
+ALTER TABLE Jugo ADD CONSTRAINT Jugo_Club
+    FOREIGN KEY (Club_Nombre, Club_Ciudad)
+    REFERENCES Club (Nombre, Ciudad)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Jugo_Jugador (table: Jugo)
+ALTER TABLE Jugo ADD CONSTRAINT Jugo_Jugador
+    FOREIGN KEY (Jugador_Nombre, Jugador_iD_Jugador)
+    REFERENCES Jugador (Nombre, iD_Jugador)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Tiene_Jugador (table: Tiene)
+ALTER TABLE Tiene ADD CONSTRAINT Tiene_Jugador
+    FOREIGN KEY (Jugador_Nombre, Jugador_iD_Jugador)
+    REFERENCES Jugador (Nombre, iD_Jugador)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Tiene_Lesion (table: Tiene)
+ALTER TABLE Tiene ADD CONSTRAINT Tiene_Lesion
+    FOREIGN KEY (Lesion_Tipo_Lesion)
+    REFERENCES Lesion (Tipo_Lesion)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- End of file.
